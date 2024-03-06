bf233625c0943b67bcb5f01d5757c8afe8adae9b
Nimi Williams <williamson.nimi@gmail.com>
2024-03-06 23:47:48 +0100

    Fixed issue with 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return Promise.all([promise]).then(() => {
-    console.log('Got a response from the API');
+  return promise.then(() => {
+      console.log('Got a response from the API');
+      return ({
+          status: 200,
+	  body: success
+      });
     /* eslint-disable no-new */
   }).catch(() => { new Error(); });
 }
