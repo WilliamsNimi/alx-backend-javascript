03136201633b7a651f7085be45f1706716ecb2b5
Nimi Williams <williamson.nimi@gmail.com>
2024-05-01 04:11:15 +0100

    made the function exportable

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,36 @@
 const fs = require('fs');
 
-const db_columns = 4;
+const dbColumns = 4;
 
-function countStudents(file_path) {
+module.exports = function countStudents(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(file_path, { encoding: 'utf8' }, (err, data) => {
+    fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const students_array = data.split('\n');
-        const std_array_length = students_array.length;
-        const num_of_students = students_array[std_array_length - 1] ? std_array_length - 1 : std_array_length - 2;
-        const student_list = ['This is the list of our students'];
-        student_list.push(`Number of students: ${num_of_students}`);
-        const course_dict = {};
-        students_array.slice(1, std_array_length).forEach((student) => {
+        const studentsArray = data.split('\n');
+        const stdArrayLength = studentsArray.length;
+        const arrayIndex = studentsArray[stdArrayLength - 1];
+        const numOfStudents = arrayIndex ? stdArrayLength - 1 : stdArrayLength - 2;
+        const studentList = ['This is the list of our students'];
+        studentList.push(`Number of students: ${numOfStudents}`);
+        const courseDict = {};
+        studentsArray.slice(1, stdArrayLength).forEach((student) => {
           if (student) {
-            const students_array = student.split(',');
-            if (course_dict[students_array[db_columns - 1]]) {
-              course_dict[students_array[db_columns - 1]].push(students_array[0]);
+            const studentsArray = student.split(',');
+            if (courseDict[studentsArray[dbColumns - 1]]) {
+              courseDict[studentsArray[dbColumns - 1]].push(studentsArray[0]);
             } else {
-              course_dict[students_array[db_columns - 1]] = [students_array[0]];
+              courseDict[studentsArray[dbColumns - 1]] = [studentsArray[0]];
             }
           }
         });
-        for (const course in course_dict) {
+        for (const course in courseDict) {
           if (course) {
-            student_list.push(`Number of students in ${course}: ${course_dict[course].length}. List: ${course_dict[course].join(', ')}`);
+            studentList.push(`Number of students in ${course}: ${courseDict[course].length}. List: ${courseDict[course].join(', ')}`);
           }
         }
-        resolve(student_list);
+        resolve(studentList);
       }
     });
   });
