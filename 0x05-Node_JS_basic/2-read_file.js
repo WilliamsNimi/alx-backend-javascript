8d09e02a670525984410e1e16f55277be7d5659c
Nimi Williams <williamson.nimi@gmail.com>
2024-05-01 04:05:22 +0100

    eslint fix on 5-http.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,44 +1,42 @@
-const fileSystem =  require('node:fs');
+const fileSystem = require('node:fs');
+
 module.exports = function countStudents(filePath) {
-    try {
-	const data = fileSystem.readFileSync(filePath, 'utf8');
-	data_list = data.toString().split('\n');
-	let f_names_course = [];
-	let first_names = [];
-	let courses = [];
-	let dict = {}
-	no_of_students = data.toString().split('\n').length - 1;
-	console.log(`Number of students: ${no_of_students}\n`)
-	for(let i = 1; i <= no_of_students; i++){
+  try {
+    const data = fileSystem.readFileSync(filePath, 'utf8');
+    data_list = data.toString().split('\n');
+    const f_names_course = [];
+    const first_names = [];
+    const courses = [];
+    const dict = {};
+    no_of_students = data.toString().split('\n').length - 1;
+    console.log(`Number of students: ${no_of_students}\n`);
+    for (let i = 1; i <= no_of_students; i++) {
 	    row = data_list[i].split(',');
 	    f_names_course.push([row[0], row[3]]);
 	    first_names.push(row[0]);
 	    courses.push(row[3]);
-	}
-	let fields = new Set(courses);
-	let fields_list = [...fields];
-	for (let i = 0; i < courses.length; i++){
-	    for (j = 0; j < fields_list.length; j++){
-		if (fields_list[j] in dict){
-		    for(const key in dict){
-			if (fields_list[j] === key){
+    }
+    const fields = new Set(courses);
+    const fields_list = [...fields];
+    for (let i = 0; i < courses.length; i++) {
+	    for (j = 0; j < fields_list.length; j++) {
+        if (fields_list[j] in dict) {
+		    for (const key in dict) {
+            if (fields_list[j] === key) {
 			    dict[fields_list[j]].push(first_names[i]);
-			}
+            }
 		    }
-		}
-		else {
+        } else {
 		    dict[fields_list[j]] = [];
 		    dict[fields_list[j]].push(first_names[i]);
-		}
+        }
 	    }
-	}
-	console.log(first_names);
-	console.log(courses);
-	console.log(fields);
-	console.log(dict);
     }
-    catch (err) {
-	console.error('Cannot load the database');
-    }
-}
-
+    console.log(first_names);
+    console.log(courses);
+    console.log(fields);
+    console.log(dict);
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
+};
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,59 +1,59 @@
 const http = require('http');
 const fs = require('fs');
 
-const db_columns = 4;
+const dbColumns = 4;
 
-function countStudents(file_path) {
+function countStudents(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(file_path, { encoding: 'utf8' }, (err, data) => {
+    fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const students_array = data.split('\n');
-        const std_array_length = students_array.length;
-        const num_of_students = students_array[std_array_length - 1] ? std_array_length - 1 : std_array_length - 2;
-        const student_list = ['This is the list of our students'];
-        student_list.push(`Number of students: ${num_of_students}`);
-        const course_dict = {};
-        students_array.slice(1, std_array_length).forEach((student) => {
+        const studentsArray = data.split('\n');
+        const stdArrayLength = studentsArray.length;
+        const arrayIndex = studentsArray[stdArrayLength - 1];
+        const numOfStudents = arrayIndex ? stdArrayLength - 1 : stdArrayLength - 2;
+        const studentList = ['This is the list of our students'];
+        studentList.push(`Number of students: ${numOfStudents}`);
+        const courseDict = {};
+        studentsArray.slice(1, stdArrayLength).forEach((student) => {
           if (student) {
-            const students_array = student.split(',');
-            if (course_dict[students_array[db_columns - 1]]) {
-              course_dict[students_array[db_columns - 1]].push(students_array[0]);
+            const studentsArray = student.split(',');
+            if (courseDict[studentsArray[dbColumns - 1]]) {
+              courseDict[studentsArray[dbColumns - 1]].push(studentsArray[0]);
             } else {
-              course_dict[students_array[db_columns - 1]] = [students_array[0]];
+              courseDict[studentsArray[dbColumns - 1]] = [studentsArray[0]];
             }
           }
         });
-        for (const course in course_dict) {
+        for (const course in courseDict) {
           if (course) {
-            student_list.push(`Number of students in ${course}: ${course_dict[course].length}. List: ${course_dict[course].join(', ')}`);
+            studentList.push(`Number of students in ${course}: ${courseDict[course].length}. List: ${courseDict[course].join(', ')}`);
           }
         }
-        resolve(student_list);
+        resolve(studentList);
       }
     });
   });
 }
 
-
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/') {
-	res.write('Hello Holberton School!');}
-    else if (req.url === '/students'){
-	try {
-	    const lists = await countStudents(process.argv[2]);
-	    lists.forEach((str, idx) => {
-		const strAug = idx > 0 ? `\n${str}` : str;
-		res.write(strAug);
-	    });
-	} catch (err) {
-	    res.write('Cannot load the database');
-	}
-	res.end();
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    try {
+      const lists = await countStudents(process.argv[2]);
+      lists.forEach((str, idx) => {
+        const strAug = idx > 0 ? `\n${str}` : str;
+        res.write(strAug);
+      });
+    } catch (err) {
+      res.write('Cannot load the database');
     }
+    res.end();
+  }
   res.end();
 }).listen(1245);
 
