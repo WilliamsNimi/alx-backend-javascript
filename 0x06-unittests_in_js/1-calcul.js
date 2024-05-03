bcaa6c30b93cd68d90fca94eecb140a6a70a51e0
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 09:05:02 +0100

    Increased number of test cases and function rework

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,11 +1,13 @@
 module.exports = function calculateNumber(type, a, b){
+    let a_round = Math.round(a);
+    let b_round = Math.round(b);
     if(type === 'SUM'){
-	return Math.ceil(a + b);
+	return a_round + b_round;
     }
     else if(type === 'SUBTRACT'){
-	return Math.ceil(a - b);
+	return a_round - b_round;
     }
     else if(type === 'DIVIDE'){
-	return Math.ceil(a / b);
+	return Math.round(a_round / b_round);
     }
 }
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,17 +2,35 @@ const calculateNumber = require('./1-calcul.js');
 const assert = require('assert');
 describe('calculateNumber function', () => {
     it('Should add two numbers and round to the nearest integer', () => {
-        const result = calculateNumber('SUM', 2.3, 5.6);
-	assert.equal(result, 8);
+        let result = calculateNumber('SUM', 1.0, 0);
+	assert.equal(result, 1);
+	result = calculateNumber('SUM', 0, 1.0);
+	assert.equal(result, 1);
+	result = calculateNumber('SUM', 1.3, 1.8);
+	assert.equal(result, 3);
+	result = calculateNumber('SUM', 1.5, 1.8);
+	assert.equal(result, 4);
     });
     
     it('Should subtract two numbers and round to the nearest integer', () => {
-        const result = calculateNumber('SUBTRACT', 2.3, 5.6);
-	assert.equal(result, -3);
+        let result = calculateNumber('SUBTRACT', 1.0, 0);
+	assert.equal(result, 1);
+	result = calculateNumber('SUBTRACT', 0, 1.0);
+	assert.equal(result, -1);
+	result = calculateNumber('SUBTRACT', 1.3, 1.8);
+	assert.equal(result, -1);
+	result = calculateNumber('SUBTRACT', 1.5, 1.8);
+	assert.equal(result, 0);
     });
     
     it('Should divide two numbers and round to the nearest integer', () => {
-        const result = calculateNumber('DIVIDE', 2.3, 5.6);
+        let result = calculateNumber('DIVIDE', 2.3, 5.6);
 	assert.equal(result, 0);
+	result = calculateNumber('DIVIDE', 2, 5.6);
+	assert.equal(result, 0);
+	result = calculateNumber('DIVIDE', 5.6, 2);
+	assert.equal(result, 3);
+	result = calculateNumber('DIVIDE', 5.6, 2.5);
+	assert.equal(result, 2);
     });
 });
