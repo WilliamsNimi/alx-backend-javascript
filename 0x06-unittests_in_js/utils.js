e24afc4531c977e080b6d2a749c56fdbbeaa4ef0
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 09:45:34 +0100

    Function rework

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,14 +1,19 @@
 const Utils = {};
 
 Utils.calculateNumber = function calculateNumber(type, a, b){
+    let a_round = Math.round(a);
+    let b_round = Math.round(b);
     if(type === 'SUM'){
-	return Math.round(a + b);
+	return a_round + b_round;
     }
     else if(type === 'SUBTRACT'){
-	return Math.round(a - b);
+	return a_round - b_round;
     }
     else if(type === 'DIVIDE'){
-	return Math.round(a / b);
+	if(b_round === 0){
+	    return 'error';
+	}
+	return Math.round(a_round / b_round);
     }
 };
 
