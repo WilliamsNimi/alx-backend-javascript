3437edeef417016d48700b448dbc9b55b32357e1
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 01:03:42 +0100

    Stub fix

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,14 +6,13 @@ const utils = require('./utils');
 describe('sendPaymentRequestToApi', () => {
     it('Should call the calculateNumber function with stub', () => {
         const stub = sinon.stub(utils, 'calculateNumber').returns(10)
-	const console = sinon.spy(console, 'log');
+	sinon.spy(console, 'log');
 
 	sendPaymentRequestToApi(100, 20);
 
-	expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-	excpect(console.calledOnceWithExactly('The total is: 10')).to.be.true;
+	expect(stub.calledOnce).to.be.true;
+	expect(stub.calledWithExactly('SUM', 100, 20)).to.be.true;
 
 	stub.restore();
-	console.restore();
     });
 });
