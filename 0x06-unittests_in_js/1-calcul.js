d24e70b41af7aa0dbf389a9f5a021658e8f291ed
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 22:54:46 +0100

    Added a new condition for divide

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -8,7 +8,7 @@ module.exports = function calculateNumber(type, a, b){
 	return a_round - b_round;
     }
     else if(type === 'DIVIDE'){
-	if(b_round === 0){
+	if(b_round === 0 || b === 0){
 	    return 'error';
 	}
 	return Math.round(a_round / b_round);
