3cfad1bd58be812a1e982589075041c1025bb303
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 22:25:12 +0100

    bodyparser update

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -4,6 +4,7 @@ const port = 7865;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
