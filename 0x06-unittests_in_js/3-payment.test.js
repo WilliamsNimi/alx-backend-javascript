d633685bddfcbf975afdac72a9cc5895d840fe20
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 01:46:20 +0100

    ConsoleSpy fix and hooks to clean up test environment

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,16 +6,15 @@ const utils = require('./utils');
 describe('sendPaymentRequestToApi', () => {
     it('Should call the calculateNumber function with spy', () => {
         const spy = sinon.spy(utils, 'calculateNumber')
-	sinon.spy(console, 'log');
+	const consoleSpy = sinon.spy(console, 'log');
 
 	sendPaymentRequestToApi(100, 20);
 
 	expect(spy.calledOnce).to.be.true;
 	expect(spy.calledWithExactly('SUM', 100, 20)).to.be.true;
-	//expect(console.calledOnce).to.be.true;
-	//expect(console.calledWithExactly('The total is: 120')).to.be.true;
+	expect(consoleSpy.calledOnce).to.be.true;
 
 	spy.restore();
-	//console.restore();
+	consoleSpy.restore();
     });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,13 +6,15 @@ const utils = require('./utils');
 describe('sendPaymentRequestToApi', () => {
     it('Should call the calculateNumber function with stub', () => {
         const stub = sinon.stub(utils, 'calculateNumber').returns(10)
-	sinon.spy(console, 'log');
+	const consoleSpy = sinon.spy(console, 'log');
 
 	sendPaymentRequestToApi(100, 20);
 
 	expect(stub.calledOnce).to.be.true;
 	expect(stub.calledWithExactly('SUM', 100, 20)).to.be.true;
+	expect(consoleSpy.calledOnce).to.be.true;
 
 	stub.restore();
+	consoleSpy.restore();
     });
 });
