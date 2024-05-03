343197071c0505249ca254438d5b1df60b1c5b50
Nimi Williams <williamson.nimi@gmail.com>
2024-05-03 08:21:30 +0100

    added more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,17 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 describe('calculateNumber function', () => {
     it('Should add two numbers and round to the nearest integer', () => {
-        const result = calculateNumber(2.3, 5.6);
+        let result = calculateNumber(2.3, 5.6);
 	assert.equal(result, 8);
+	result = calculateNumber(2, 4);
+	assert.equal(result, 6);
+	result = calculateNumber(-1, -2);
+	assert.equal(result, -3);
+	result = calculateNumber(0.0, 0.0);
+	assert.equal(result, 0.0);
+	result = calculateNumber(1, 3.7);
+	assert.equal(result, 5);
+	result = calculateNumber(1.5, 3.7);
+	assert.equal(result, 6);
     });
 });
