cfc649600b4603feb2a4dc8ef278edf207ca4637
Nimi Williams <williamson.nimi@gmail.com>
2024-05-26 11:12:29 +0100

    Cleanup

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,39 +1,3 @@
-/*const express = require('express');
-const app = express();
-const port = 7865;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
-});
-
-app.get('/cart/:id', (req, res) => {
-    if (!Number.isNaN(Number(req.params.id))){
-	res.send(`Payment methods for cart ${req.params.id}`);
-    } else {
-	res.status(404).send('Cart not found');
-    }
-});
-app.get('/available_payments', (req, res) => {
-let available_payments = {
-  payment_methods: {
-    credit_cards: true,
-    paypal: false
-  }
-}
-res.send(available_payments);
-});
-app.post('/login', (req, res) => {
-    const username = req.body.userName;
-    res.send(`Welcome ${username}`);
-});
-
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
-});*/
 const express = require('express');
 const bodyParser = require('body-parser');
 
