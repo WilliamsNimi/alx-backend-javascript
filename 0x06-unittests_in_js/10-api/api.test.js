a574d979f0ed1592108c4a58cf9a9bbe929fa873
Nimi Williams <williamson.nimi@gmail.com>
2024-05-26 11:06:07 +0100

    updating the api_test suit

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+/*const { expect } = require('chai');
 const request = require('request');
 
 describe('Index page test', () => {
@@ -61,4 +61,137 @@ describe('Index page test', () => {
 	    done();
 	});
      });
+});
+*/
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('returns a string', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns 404', (done) => {
+    request('http://localhost:7865/nope', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('returns 404', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request('http://localhost:7865/cart/1', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 1');
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request('http://localhost:7865/cart/12345', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12345');
+      done();
+    });
+  });
+
+  it('returns 404', (done) => {
+    request('http://localhost:7865/cart/123ab', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('returns 404', (done) => {
+    request('http://localhost:7865/cart/12/34', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('returns 200', (done) => {
+    request(
+      'http://localhost:7865/available_payments',
+      (error, response, body) => {
+        expect(body).to.equal(
+          '{"payment_methods":{"credit_cards":true,"paypal":false}}'
+        );
+        done();
+      }
+    );
+  });
+
+  it('returns 200', (done) => {
+    request(
+      'http://localhost:7865/available_payments',
+      (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+
+  it('returns 200', (done) => {
+    request(
+      {
+        url: 'http://localhost:7865/login',
+        method: 'POST',
+        form: { userName: 'Betty' }
+      },
+      (error, response, body) => {
+        expect(body).to.equal('Welcome Betty');
+        done();
+      }
+    );
+  });
+
+  it('returns 200', (done) => {
+    request.post(
+      'http://localhost:7865/login',
+      { form: { userName: 'Betty' } },
+      (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+
+  it('returns 200', (done) => {
+    request.post(
+      'http://localhost:7865/login',
+      { form: { userName: 'Mic' } },
+      (error, response, body) => {
+        expect(body).to.equal('Welcome Mic');
+        done();
+      }
+    );
+  });
 });
